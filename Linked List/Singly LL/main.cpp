2b60b6666e60ab9e2e76c87d88af94708856b3f5
meet-salman <say2salmanahmed@gmail.com>
2025-12-05 19:08:59 +0500

    find_length

--- Linked List/Singly LL/main.cpp
+++ Linked List/Singly LL/main.cpp
@@ -16,6 +16,24 @@ struct LinkedList
     // Constructor to initialize head & tail = Null
     LinkedList() : head(nullptr), tail(nullptr) {}
 
+    // Find length of LL
+    int find_length()
+    {
+        int count = 0;
+        if (!head)
+            return count;
+        else
+        {
+            Node *current = head;
+            while (current)
+            {
+                count++;
+                current = current->next;
+            }
+        }
+        return count;
+    }
+
     //  Element added at last
     void push_back(string val)
     {
@@ -46,11 +64,22 @@ struct LinkedList
     // Element added at position (1 - x)
     void push_at_position(int position, string val)
     {
-        Node *n = new Node(val);
+        int len = find_length();
+        if (position < 0 || position > len)
+        {
+            cout << "Invalid Position" << endl;
+            return;
+        }
 
+        Node *n = new Node(val);
         if (position == 0)
         {
             push_front(val);
+            return;
+        }
+        if (position == len)
+        {
+            push_back(val);
             return;
         }
 
@@ -60,13 +89,6 @@ struct LinkedList
         {
             idx++;
             current = current->next;
-        }
-
-        if (!current || position < 0)
-        {
-            cout << "Invalid Position" << endl;
-            delete n;
-            return;
         }
 
         n->next = current->next;
@@ -105,7 +127,7 @@ main()
     list.push_back("9");
     list.push_back("10");
 
-    list.push_at_position(-1, "5");
+    list.push_at_position(0, "5");
 
     list.display_list();
 
