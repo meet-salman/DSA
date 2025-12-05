7db4ed40401635498973995ffe08bbacd498fcfb
meet-salman <say2salmanahmed@gmail.com>
2025-12-06 01:23:27 +0500

    push_back

--- Linked List/Circular LL/main.cpp
+++ Linked List/Circular LL/main.cpp
@@ -10,16 +10,46 @@ struct Node
     Node(string val) : value(val), next(nullptr), previous(nullptr) {}
 };
 
-struct LinkedList
+//  Element added at last
+void push_back(Node *&head, Node *&tail, string val)
 {
-    Node *head, *tail;
+    Node *n = new Node(val);
+    if (!head)
+    {
 
-    // Constructor to initialize head & tail = Null
-    LinkedList() : head(nullptr), tail(nullptr) {}
-};
+        head = tail = n;
+        tail->next = head->previous = n; // circular
+    }
+    else
+    {
+        n->previous = tail;
+        n->next = head;     // circular
+        head->previous = n; // circular
+        tail->next = n;
+        tail = n;
+    }
+}
 
-main()
+int main()
 {
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    push_back(head, tail, "6");
+    push_back(head, tail, "7");
+    push_back(head, tail, "8");
+    push_back(head, tail, "9");
+
+    cout << head << " " << head->next->value << " " << head->value << endl;
+    cout << tail << " " << tail->next->value << " " << tail->value << endl;
+
+    Node *current = head;
+    cout << endl;
+    do
+    {
+        cout << current->value << " ";
+        current = current->next;
+    } while (current != head);
 
     return 0;
 }
