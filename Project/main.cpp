4cfdf227bc05e12f63d1672cef3b48117fe0e2f8
meet-salman <say2salmanahmed@gmail.com>
2025-12-21 23:24:57 +0500

    Converted to class based structure

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,56 +1,84 @@
 #include "template.h"
 
-void add_link(vec_2d_pair_iint &network, int &networkSize)
+class Network
 {
-    int router, next, cost;
-    cin >> router >> next >> cost;
+private:
+    string networkName;
+    int noOfRouters, links;
+    vec_2d_pair_iint network;
 
-    if (router >= networkSize || next >= networkSize)
+public:
+    Network(string networkName)
     {
-        cout << "Out of network size\n";
-        return;
+        this->noOfRouters = 0;
+        this->links = 0;
+        this->networkName = networkName;
+        cout << "Network established successsfully\n";
+        cout << "Network size: " << noOfRouters << endl;
     }
 
-    network[router].push_back({next, cost});
-    network[next].push_back({router, cost});
-}
-
-void add_router(vec_2d_pair_iint &network, int &networkSize)
-{
-    network.resize(++networkSize);
-}
-
-void display_network(vec_2d_pair_iint &network, int &networkSize)
-{
-    for (int i = 1; i < networkSize; i++)
+    void network_size()
     {
-        cout << i << " -> ";
-        for (auto node : network[i])
-            cout << "{" << node.first << ", " << node.second << "} ";
+        cout << "network size: " << noOfRouters << " Routers, " << links << " Links" << endl;
+    }
+
+    void add_routers()
+    {
+        int routers;
+        cin >> routers;
+        noOfRouters += routers;
+        network.resize(noOfRouters);
+
+        cout << "\nNetwork expanded successfully with " << routers << " routers\n";
+        network_size();
+    }
+
+    void add_links()
+    {
+        int edges;
+        cin >> edges;
 
         cout << endl;
+        while (edges--)
+        {
+            int router, next, cost;
+            cin >> router >> next >> cost;
+
+            if (router >= noOfRouters || router < 0 || next >= noOfRouters || next < 0)
+            {
+                cout << router << "—" << next << " Out of network\n";
+                continue;
+            }
+
+            if (cost < 0)
+            {
+                cout << "Invalid cost at: " << router << "—" << next << endl;
+                continue;
+            }
+
+            network[router].push_back({next, cost});
+            network[next].push_back({router, cost});
+
+            cout << "Router " << router << " & " << next << " linked successfully\n";
+            links++;
+        }
+        network_size();
     }
-}
 
-void create_network()
-{
-    int networkSize, links;
-    cin >> networkSize >> links;
-    vec_2d_pair_iint network((++networkSize));
-
-    int router, next, cost;
-    int i = links;
-    while (i--)
+    void display_network()
     {
-        cin >> router >> next >> cost;
-        network[router].push_back({next, cost});
-        network[next].push_back({router, cost});
-    }
+        cout << endl;
+        for (int i = 0; i < noOfRouters; i++)
+        {
+            // cout <<"Router " <<i << " -> ";
+            cout << i << " -> ";
+            for (auto node : network[i])
+                cout << "{" << node.first << ", " << node.second << "} ";
 
-    add_router(network, networkSize);
-    add_link(network, networkSize);
-    display_network(network, networkSize);
-}
+            cout << endl;
+        }
+    }
+};
 
 int main()
 {
@@ -59,18 +87,30 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    create_network();
+    Network n("CS Dept");
+    n.add_routers();
+    n.add_links();
+    n.display_network();
+    n.add_routers();
+    n.add_links();
+    n.display_network();
 
     // ------------
     // Sample Input
-    // 5 6
-    // 1 2 3
-    // 1 3 5
-    // 2 3 4
-    // 3 4 6
-    // 2 5 2
-    // 4 5 1
+    // 6
+    // 7
+    // 0 2 5
+    // 0 1 3
+    // 1 3 7
+    // 2 3 6
+    // 2 4 1
+    // 3 5 8
+    // 4 5 2
+    // 1
+    // 3
     // 6 2 5
+    // 5 6 7
+    // 1 4 1
     // ------------
 
     return 0;
