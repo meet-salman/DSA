8d0d8e78b52f3745f04115370f9ccb5589e6d263
meet-salman <say2salmanahmed@gmail.com>
2025-12-21 21:29:58 +0500

    update

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,25 +1,31 @@
 #include "template.h"
 
-void add_link(vec_2d_pair_iint &graph)
+void add_link(vec_2d_pair_iint &network, int &networkSize)
 {
     int router, next, cost;
     cin >> router >> next >> cost;
 
-    graph[router].push_back({next, cost});
-    graph[next].push_back({router, cost});
+    if (router >= networkSize || next >= networkSize)
+    {
+        cout << "Out of network size\n";
+        return;
+    }
+
+    network[router].push_back({next, cost});
+    network[next].push_back({router, cost});
 }
 
-void add_router(vec_2d_pair_iint &graph)
+void add_router(vec_2d_pair_iint &network, int &networkSize)
 {
-    graph.resize(graph.size() + 1);
+    network.resize(++networkSize);
 }
 
-void display_network(vec_2d_pair_iint &graph)
+void display_network(vec_2d_pair_iint &network, int &networkSize)
 {
-    for (int i = 1; i < graph.size(); i++)
+    for (int i = 1; i < networkSize; i++)
     {
         cout << i << " -> ";
-        for (auto node : graph[i])
+        for (auto node : network[i])
             cout << "{" << node.first << ", " << node.second << "} ";
 
         cout << endl;
@@ -28,22 +34,22 @@ void display_network(vec_2d_pair_iint &graph)
 
 void create_network()
 {
-    int nodes, edges;
-    cin >> nodes >> edges;
-    vec_2d_pair_iint graph((nodes + 1));
+    int networkSize, links;
+    cin >> networkSize >> links;
+    vec_2d_pair_iint network((++networkSize));
 
     int router, next, cost;
-    int i = edges;
+    int i = links;
     while (i--)
     {
         cin >> router >> next >> cost;
-        graph[router].push_back({next, cost});
-        graph[next].push_back({router, cost});
+        network[router].push_back({next, cost});
+        network[next].push_back({router, cost});
     }
 
-    add_router(graph);
-    add_link(graph);
-    display_network(graph);
+    add_router(network, networkSize);
+    add_link(network, networkSize);
+    display_network(network, networkSize);
 }
 
 int main()
@@ -53,10 +59,7 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    int t = 1;
-    // cin >> t;
-    while (t--)
-        create_network();
+    create_network();
 
     // ------------
     // Sample Input
@@ -67,7 +70,7 @@ int main()
     // 3 4 6
     // 2 5 2
     // 4 5 1
-    // 6 1 5
+    // 6 2 5
     // ------------
 
     return 0;
