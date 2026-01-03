a025b13497f761a820fdee5a31e7f63f6e6f33f0
meet-salman <say2salmanahmed@gmail.com>
2026-01-03 16:52:08 +0500

    Implement binary tree construction using recursion

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,4 +1,5 @@
-#include "template.h"
+#include <bits/stdc++.h>
+using namespace std;
 
 struct Node
 {
@@ -9,73 +10,29 @@ struct Node
     Node(int val) : val(val), left(nullptr), right(nullptr) {}
 };
 
-struct Tree
+Node *create_node()
 {
-    Node *root;
-    Tree() : root(nullptr) {}
+    int val;
+    cin >> val;
 
-    void add_root(int val)
-    {
-        if (!root)
-        {
-            Node *n = new Node(val);
-            root = n;
+    Node *temp = new Node(val);
 
-            cout << (n->left ? to_string(n->left->val) : "null") << " "
-                 << n->val << " "
-                 << (n->right ? to_string(n->right->val) : "null") << "\nroot added\n\n";
-        }
-        else
-        {
-            cout << "Root already defined!\n";
-            return;
-        }
-    }
+    if (val == -1)
+        return nullptr;
 
-    void add_node(Node *parent)
-    {
-        int left, right;
-        cin >> left >> right;
+    temp->left = create_node();
+    temp->right = create_node();
 
-        if (left != -1)
-            parent->left = new Node(left);
-
-        if (right != -1)
-            parent->right = new Node(right);
-    }
-};
+    return temp;
+}
 
 void solve()
 {
-    Tree t;
-
-    queue<Node *> q;
-    int rootVal;
-    cin >> rootVal;
-
-    t.add_root(rootVal);
-    q.push(t.root);
-
-    while (!q.empty())
-    {
-        Node *parent = q.front();
-        q.pop();
-
-        t.add_node(parent);
-        if (parent->left != nullptr)
-            q.push(parent->left);
-        if (parent->right != nullptr)
-            q.push(parent->right);
-
-        cout << (parent->left ? to_string(parent->left->val) : "null") << " "
-             << parent->val << " "
-             << (parent->right ? to_string(parent->right->val) : "null") << "\n";
-    }
+    Node *root = create_node();
 }
 
 int main()
 {
-    file_io();
     solve();
 
     return 0;
