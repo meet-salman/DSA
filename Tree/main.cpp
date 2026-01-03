4fede126036486927791a52923a221f3337dcc78
meet-salman <say2salmanahmed@gmail.com>
2026-01-03 19:31:05 +0500

    Implement post-order traversal

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include "template.h"
 
 struct Node
 {
@@ -26,9 +25,21 @@ Node *create_node()
     return temp;
 }
 
+void pre_order_traversal(Node *node)
+{
+    if (node == nullptr)
+        return;
+
+    // Node Left Right
+    cout << node->val << " -> ";
+    pre_order_traversal(node->left);
+    pre_order_traversal(node->right);
+}
+
 void solve()
 {
     Node *root = create_node();
+    pre_order_traversal(root);
 }
 
 int main()
